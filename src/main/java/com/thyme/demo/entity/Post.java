211bfc6845901c0a4822189e32fdcab848037af3
John Doe <johndoe@on.net>
2026-03-26 20:32:42 +0900

    entity col type

--- src/main/java/com/thyme/demo/entity/Post.java
+++ src/main/java/com/thyme/demo/entity/Post.java
@@ -33,7 +33,7 @@ public class Post {
     @Column(nullable = false)
     private String title;
     private String url;
-    @Lob
+
     @Column(nullable = false)
     private String content;
     private String shortDescription;
